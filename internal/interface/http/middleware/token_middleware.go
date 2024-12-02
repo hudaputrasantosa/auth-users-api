efae2743773f757b18950f1124f42e1b7c5fde7d
hudaputrasantosa <hudaputrasantosa@gmail.com>
2024-12-03 00:25:22 +0700

    feat(token): add middleware for jwt protected

---
+++ internal/interface/http/middleware/token_middleware.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	jwtMiddleware "github.com/gofiber/contrib/jwt"
+
+	"github.com/hudaputrasantosa/auth-users-api/internal/config"
+	"github.com/hudaputrasantosa/auth-users-api/pkg/helper/response"
+)
+
+// JWTProtected func for specify routes group with JWT authentication.
+// See: https://github.com/gofiber/contrib/jwt
+func JWTProtected() func(*fiber.Ctx) error {
+	// Create config for JWT authentication middleware.
+	config := jwtMiddleware.Config{
+		SigningKey:   jwtMiddleware.SigningKey{Key: []byte(config.Config("JWT_SECRET_KEY"))},
+		ContextKey:   "jwt", // used in private routes
+		ErrorHandler: jwtError,
+	}
+
+	return jwtMiddleware.New(config)
+}
+
+func jwtError(c *fiber.Ctx, err error) error {
+	// Return status 401 and failed authentication error.
+	if err.Error() == "Missing or malformed JWT" {
+		return response.ErrorMessage(c, fiber.StatusBadRequest, err.Error(), nil)
+	}
+
+	// Return status 401 and failed authentication error.
+	return response.ErrorMessage(c, fiber.StatusUnauthorized, err.Error(), nil)
+}
